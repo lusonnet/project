6f90cf77c247dbed42f836a164c5faa00db91020
sonnet <lulu_2001@foxmail.com>
2021-06-22 23:16:26 +0800

    提交代码

--- toilet manage/toilet manage/test.c
+++ toilet manage/toilet manage/test.c
@@ -1,14 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#define PASSWORD 123456//通行码
+
 
 typedef struct EN//存用户和密码 
 {
-	char userName1[10];
-	char passWorld1[10];
+	char username[20];
+	char passworld[20];
 }EN;
+
+typedef struct FOOD//食物
+{
+	char name[20];//菜品名
+	float price;//菜品价格
+	struct FOOD* next;
+	int num;//菜品序号
+}FOOD;
+
+int Choice()//选择函数
+{
+	int choice;
+	printf("请选择：>");
+	scanf("%d", &choice);
+	return choice;
+}
+
+void enter();//空函数
 
 void Primarymenu()
 {
-	printf("----欢迎来到厕所管理系统----\n");
+	printf("----欢迎来到餐厅管理系统----\n");
 	printf("--------您的身份-----------\n");
 	printf("--------1.管理员-----------\n");
 	printf("--------2.用户-------------\n");
@@ -23,14 +45,46 @@ void secondMenu()
 	printf("--------0.返回-------------\n");
 }
 
+void allowMenu()
+{
+	printf("--------1.增加菜品----------\n");
+	printf("--------2.删除菜品----------\n");
+	printf("--------3.查询菜品----------\n");
+	printf("--------4.修改菜品价格----------\n");
+}
+
 int main()
 {
-	int input;
 	int choice;
-	Primarymenu();
-	scanf("%d", &input);
+
 	do
 	{
+		Primarymenu();//选择身份 - 1.管理员2.用户
+		choice = Choice();
+		switch (choice)
+		{
+		case 1://管理员
+		{
+			char allow[10];
+			printf("请输入验证码：>");
+			scanf("%s", allow);
+			if (strcmp(allow, "PASSWORD"))//通行码错误则进入函数
+			{
+				printf("通行码错误-_-\n");
+				enter();
+			}
+			printf("\t");
+			allowMenu();//增删查改
+			choice = Choice();
+			switch(choice)
+			{
+			case 1: {//增加菜品
 
-	} while (input);
+			}
+				
+
+			}
+		}
+		}
+	} while (choice);
 }
